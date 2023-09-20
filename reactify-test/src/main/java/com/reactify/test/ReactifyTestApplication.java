946c6eec6636cbdf5a90ac9ca9858c7e77768057
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2023-09-20 12:00:00 +0000

    reactify-core

--- reactify-test/src/main/java/com/reactify/test/ReactifyTestApplication.java
+++ reactify-test/src/main/java/com/reactify/test/ReactifyTestApplication.java
@@ -17,10 +17,8 @@ package com.reactify.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.reactify.test", "com.reactify"})
 public class ReactifyTestApplication {
 
 	public static void main(String[] args) {
